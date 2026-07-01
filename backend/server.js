616b3c7b819cf96288ac4cd14842e0b194daaf61
azmainic <aiakibchowdhury@gmail.com>
2026-07-01 15:17:06 +0100

    Save local changes before pull

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173',
-  'http://localhost:5000',
+  'http://localhost:5001',
   'https://parkin-web.azurewebsites.net',
   'https://parkin-api.azurewebsites.net'
 ];
